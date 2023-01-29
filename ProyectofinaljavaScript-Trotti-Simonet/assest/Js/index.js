8a0e5ac83f1a7ed04c409acc87527e2711744c03
CoderLtrotti <120693492+CoderLtrotti@users.noreply.github.com>
2023-01-29 20:20:04 -0300

    Create index.js

---
+++ ProyectofinaljavaScript-Trotti-Simonet/assest/Js/index.js
@@ -0,0 +1,21 @@
+//Variables
+let nombre = "Luciano"; //declarar la variable nombre y asignar el valor tipo string
+let apellido = " Trotti Simonet";
+let nombrecompleto = "Mi nombre es "
+let pagina = "Proyecto Jugueteria Kids"
+let edad = 32; //variable del tipo integer
+let altura = 1.77;
+let resultado = nombrecompleto + nombre + apellido;
+
+//MENSAJE PRINCIPAL
+alert(resultado);
+alert(pagina);
+alert("Proyecto Jugueteria Kids");
+
+
+
+
+
+
+
+
