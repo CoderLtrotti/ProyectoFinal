1beb6ed33753914f4ebfe30e29d6e71ab0297d40
CoderLtrotti <120693492+CoderLtrotti@users.noreply.github.com>
2023-01-29 20:20:13 -0300

    Create productosmain.js

---
+++ ProyectofinaljavaScript-Trotti-Simonet/assest/Js/productosmain.js
@@ -0,0 +1,24 @@
+const renderProductos = () => {
+    const productos = cargarProductosLS();
+    let salida = "";
+  
+    for (let producto of productos) {
+        salida += ` <div class="col-md-3">
+                <div class="card text-center">
+                    <img src="images/${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
+                    <div class="card-body">
+                    <h5 class="card-title">${producto.nombre}</h5>
+                    <p class="card-text">${producto.precio}</p>
+                    <a href="#" class="btn btn-primary" onclick="agregarALCarrito(${producto.id});">Agregar al Carrito</a>
+                    </div>
+                </div>
+            </div>`;
+    }
+
+    document.getElementById("productos").innerHTML = salida;
+}
+  
+  renderProductos();
+  renderBotonCarrito();
+
+
