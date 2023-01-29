1f9882522a07a5bf67e60854e2c990e8e221ef2c
CoderLtrotti <120693492+CoderLtrotti@users.noreply.github.com>
2023-01-29 20:20:14 -0300

    Create promociones.js

---
+++ ProyectofinaljavaScript-Trotti-Simonet/assest/Js/promociones.js
@@ -0,0 +1,17 @@
+    //suma promociones
+    let nombre = "Primera Promocion="
+    let nombre2 = "Segunda Promocion="
+    let nombre3 = "$"
+
+    //variables
+    let promocion1 = 45
+    let promocion2 = 69
+    let resultado = promocion1
+    let resultado2 = promocion2
+    alert(nombre + nombre3 + resultado)
+    alert(nombre2 + nombre3 + resultado2)
+
+    //alert, console y prompt
+
+    let nombreIngresado = prompt("Ingrese Promocion")
+    alert("Tu Promocion es : " + nombreIngresado)
